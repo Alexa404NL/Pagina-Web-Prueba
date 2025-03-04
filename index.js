d0f258093ff40013d2f33f8f794ff624cd864ca3
Alexa404NL <66581333+Alexa404NL@users.noreply.github.com>
2025-03-03 19:36:50 -0600

    arreglos path

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const app = express();
-const port =4000;
+const port =5000;
 
-app.use("/", express.static("public"));
+app.use("/", express.static("public/Pag_Web_2025-main"));
 app.listen(port, console.log("http://localhost:"+ port));
 
 
